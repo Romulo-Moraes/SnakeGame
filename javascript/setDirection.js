f6ff66572f849944f35777d4b859b5750747a7cd
Rômulo <romuloperes6@gmail.com>
2021-07-24 15:08:52 -0300

    bugs fixed

--- javascript/setDirection.js
+++ javascript/setDirection.js
@@ -1,19 +1,25 @@
 window.addEventListener("keydown", (key) => {
     if(key.keyCode == "87"){
-        SnakeBody[0].LastDirection = "Up"
+        if(SnakeBody[0].LastDirection != "Down"){
+            SnakeBody[0].LastDirection = "Up"
+        }
     }
     if(key.keyCode == "68"){
-        SnakeBody[0].LastDirection = "Right"
+        if(SnakeBody[0].LastDirection != "Left"){
+            SnakeBody[0].LastDirection = "Right"
+        }
     }
     if(key.keyCode == "65"){
-        SnakeBody[0].LastDirection = "Left"
+        if(SnakeBody[0].LastDirection != "Right"){
+            SnakeBody[0].LastDirection = "Left"
+        }
     }
     if(key.keyCode == "83"){
-        SnakeBody[0].LastDirection = "Down"
+        if(SnakeBody[0].LastDirection != "Up"){
+            SnakeBody[0].LastDirection = "Down"
+        }
     }
     if(key.keyCode == "80"){
-        //window.alert(AllBody[0].PositionX + " " +  FruitPositionX + " " + AllBody[0].PositionY + " " + FruitPostionY)
-        //window.alert(typeof(AllBody[0].PositionX) + " " + typeof(AllBody[0].PositionY))
         let bodyCount = document.getElementsByClassName("Body")
         window.alert(JSON.stringify(SnakeBody))
     }
